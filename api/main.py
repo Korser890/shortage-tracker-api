c55ac64fe9fa005b9ab2b654e13b8bae44f2759a
Korser890 <axelmatthew47@gmail.com>
2026-06-05 23:10:09 +1000

    deploy: CORS env var, fix requirements.txt encoding, add Procfile

--- api/main.py
+++ api/main.py
@@ -1,12 +1,15 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routers import health, overview, commodity
 
 app = FastAPI(title="Global Resource Shortage Tracker API")
 
+origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_methods=["GET"],
     allow_headers=["*"],
 )
